8d58d286b0aa9b1df8f2f5787cbedf0dda54c9bf
BigChocoMonster <bigchocomonster@gmail.com>
2020-12-17 20:34:06 +0530

    - drawing input feedback to canvas

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
+// Register service worker
 window.addEventListener('load', () => {
   if ('serviceWorker' in navigator) {
-    // browser checks if already registered and handles accordingly across all loads
+    // Browser checks if already registered and handles accordingly across all loads
     navigator.serviceWorker.register('service-worker.js').then(
       (registration) => {
         console.log(
           '%c Success bitches!',
           'color: #24fc03',
           'Scope: ' + registration.scope
-        ); // successful registration
+        ); // Successful registration
       },
       (error) => {
-        console.error('Whoops!', 'Error: ' + error); // failed registration
+        console.error('Whoops!', 'Error: ' + error); //Failed registration
       }
     );
   }
 });
+
+// Stopping weird form submission page reloads
+document.forms.signature.addEventListener('submit', (event) => {
+  event.preventDefault();
+});
+
+// Initialising canvas text
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+context.font = '70px BillionReach';
+context.textBaseline = 'middle';
+context.textAlign = 'center';
+
+// Drawing to canvas on font load
+const font = new FontFace(
+  'BillionReach',
+  'url(./fonts/billion_reach/BillionReach.otf)'
+);
+font.load().then(() => {
+  writeToCanvas(document.forms.signature.name.value);
+
+  document.forms.signature.name.addEventListener('input', (event) => {
+    writeToCanvas(event.target.value);
+  });
+});
+
+function writeToCanvas(text) {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.fillText(text || 'I am neu-d', canvas.width / 2, canvas.height / 2);
+}
